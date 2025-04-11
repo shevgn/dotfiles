d89c4499255dd0c7f57b0159e3054a52fc4fae6b
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-04-11 13:52:15 +0300

    htmx formatter & linter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,17 +1,47 @@
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				go = { "goimports", "gofmt" },
-				rust = { "rustfmt", lsp_format = "fallback" },
-				vue = { "prettier", "eslint_d" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
-		})
-	end,
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      formatters = {
+        sleek = {
+          command = "sleek",
+        }
+      },
+      formatters_by_ft = {
+        go = { "goimports", "gofmt" },
+        rust = { "rustfmt", lsp_format = "fallback" },
+        vue = {
+          "prettier",
+          "eslint_d",
+        },
+        svelte = {
+          "prettier",
+          "eslint_d",
+        },
+        htmx = {
+          "prettier",
+          "eslint_d",
+        },
+        typescript = {
+          "prettier",
+          "eslint_d",
+        },
+        javascript = {
+          "prettier",
+          "eslint_d",
+        },
+        json = {
+          "prettier",
+        },
+        sql = {
+          "sleek",
+        }
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
+  end,
 }
